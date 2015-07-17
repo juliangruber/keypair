b9e4e254a04a5405837356b3696aef1bba21d314
Petro Podrezo <Petro.Podrezo@td.com>
2015-07-17 15:37:57 -0400

    Added authorized_keys functionality

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+var crypto = require('crypto');
 var keypair = require('./index.js');
 var test = require('tape');
 
@@ -7,5 +8,20 @@ test('keypair', function (t) {
   t.assert(/BEGIN RSA PRIVATE KEY/.test(pair.private), 'private header');
   t.ok(pair.public, 'public key');
   t.assert(/BEGIN RSA PUBLIC KEY/.test(pair.public), 'public header');
+  t.end();
+});
+
+test('test against crypto library', function(t) {
+  var pair = keypair();
+  var testString = 'Hello, world'
+  var encrypted = crypto.publicEncrypt(pair.public, new Buffer(testString));
+  var decrypted = crypto.privateDecrypt(pair.private, encrypted);
+  t.equal(decrypted.toString(), testString);
+  t.end();
+});
+
+test('ensure OpenSSH line is generated when the option is passed', function(t) {
+  var pair = keypair({openSSH: true});
+  t.ok(pair.openSSH, 'OpenSSH');
   t.end();
 });
