38537374a94abdef92507cda81abb5c45685ccb2
eskelter <mycosoft@yahoo.fr>
2017-10-14 10:19:13 -0400

    Improves type declarations

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,19 @@
-declare namespace keypair {
-    export interface Options {
-        bits?: number,
-        e?: number
-    }
-    
-    export interface Results {
-        public: string,
-        private: string
-    }    
+// Type definitions for keypair 1.0
+// Project: https://www.npmjs.com/package/keypair
+// Definitions by: eskelter <https://github.com/eskelter>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.2
+export interface KeypairOptions {
+    bits?: number;
+    e?: number;
+}
+export interface KeypairResults {
+    public: string;
+    private: string;
 }
 
 /**
  * Get an RSA PEM key pair.
  * @param opts
  */
-declare function keypair (opts?: keypair.Options): keypair.Results;
-
-export = keypair;
+export function keypair(opts?: KeypairOptions): KeypairResults;
